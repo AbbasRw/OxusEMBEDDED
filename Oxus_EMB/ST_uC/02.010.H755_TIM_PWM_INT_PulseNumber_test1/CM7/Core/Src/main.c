d7bb504a9732f5ff2be8b312366beb3256901380
abbasBiosys <abbasraimkulov@gmail.com>
2025-11-26 11:10:04 +0300

    Update main.c

--- Oxus_EMB/ST_uC/02.010.H755_TIM_PWM_INT_PulseNumber_test1/CM7/Core/Src/main.c
+++ Oxus_EMB/ST_uC/02.010.H755_TIM_PWM_INT_PulseNumber_test1/CM7/Core/Src/main.c
@@ -499,7 +499,6 @@ void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
 
         if (step_counter >= step_target)
         {
-        	DebugPrint("TIM");
             HAL_TIM_PWM_Stop_IT(&htim5, TIM_CHANNEL_4);
             is_moving = 0;
         }
